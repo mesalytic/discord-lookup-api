a6b8d24ca671391d67256863f3105addee7c2f84
Alice <atamatori56@gmail.com>
2022-08-12 04:53:15 +0200

    feat: added badges array

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/', (req, res) => {
-    res.send("root page");
-})
+app.get("/", (req, res) => {
+  res.send("root page");
+});
 
 app.get("/:id/", cors({ methods: ["GET"] }), (req, res) => {
   let id = req.params.id;
@@ -30,6 +30,29 @@ app.get("/:id/", cors({ methods: ["GET"] }), (req, res) => {
   })
     .then((res) => res.json())
     .then((json) => {
+      let publicFlags = [];
+
+      let FLAGS = [
+        { flag: "DISCORD_EMPLOYEE", bitwise: 1 << 0 },
+        { flag: "PARTNERED_SERVER_OWNER", bitwise: 1 << 1 },
+        { flag: "HYPESQUAD_EVENTS", bitwise: 1 << 2 },
+        { flag: "BUGHUNTER_LEVEL_1", bitwise: 1 << 3 },
+        { flag: "HOUSE_BRAVERY", bitwise: 1 << 6 },
+        { flag: "HOUSE_BRILLIANCE", bitwise: 1 << 7 },
+        { flag: "HOUSE_BALANCE", bitwise: 1 << 8 },
+        { flag: "EARLY_SUPPORTER", bitwise: 1 << 9 },
+        { flag: "TEAM_USER", bitwise: 1 << 10 },
+        { flag: "BUGHUNTER_LEVEL_2", bitwise: 1 << 14 },
+        { flag: "VERIFIED_BOT", bitwise: 1 << 16 },
+        { flag: "EARLY_VERIFIED_BOT_DEVELOPER", bitwise: 1 << 17 },
+        { flag: "DISCORD_CERTIFIED_MODERATOR", bitwise: 1 << 18 },
+        { flag: "BOT_HTTP_INTERACTIONS", bitwise: 1 << 19 },
+      ];
+
+      FLAGS.forEach((flag) => {
+        if (json.public_flags & flag.bitwise) publicFlags.push(flag.flag);
+      });
+
       let avatarLink = null;
       if (json.avatar)
         avatarLink = `https://cdn.discordapp.com/avatars/${json.id}/${json.avatar}`;
@@ -41,6 +64,7 @@ app.get("/:id/", cors({ methods: ["GET"] }), (req, res) => {
       res.send({
         id: json.id,
         tag: `${json.username}#${json.discriminator}`,
+        badges: publicFlags,
         avatar: {
           id: json.avatar,
           link: avatarLink,
